86f0ad328f7ea3710fafba38f275d13a0d4e7078
github-action-benchmark <github@users.noreply.github.com>
2025-09-03 22:13:29 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 45f724c4f2cef8d09ed84226bea545ac83872e6b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1756937533738,
+  "lastUpdate": 1756937609455,
   "repoUrl": "https://github.com/dirvine/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -39,6 +39,37 @@ window.BENCHMARK_DATA = {
             "name": "ant files download",
             "value": 27.28108243573729,
             "unit": "MiB/s"
+          }
+        ]
+      }
+    ],
+    "Node memory": [
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "45f724c4f2cef8d09ed84226bea545ac83872e6b",
+          "message": "Merge pull request #3178 from maidsafe/rc-2025.9.1\n\nchore(release): stable release 2025.9.1.2",
+          "timestamp": "2025-09-03T18:21:18+01:00",
+          "tree_id": "8964b8a6723ccf48b29aa49e3fc359dae62cab02",
+          "url": "https://github.com/dirvine/autonomi/commit/45f724c4f2cef8d09ed84226bea545ac83872e6b"
+        },
+        "date": 1756937609123,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 89,
+            "unit": "MB"
           }
         ]
       }
